c006709e997c1c65408c398b19797dc3aba072f2
jackdefay <3jackattack3@gmail.com>
2023-04-21 01:08:09 -0400

    when only running the simulation in paramsweep and manually running animation, the code works. Assuming there is an overflow error from holding the entire simulation in ram twice or something. Need to make sure everything is cleared first, or need to split into batch_sim and batch_animation files

--- heap/paramsweep.py
+++ heap/paramsweep.py
@@ -55,7 +55,6 @@ Fish = getattr(importlib.import_module('fishfood.' + experiment_file), 'Fish')
 ## Feel free to loop over multiple simulations with different parameters! ##
 
 # Experimental Parameters
-#TODO: change this back to 20 
 no_fish = 20
 simulation_time = 400 # [s]
 clock_freq = 2 # [Hz]
@@ -65,9 +64,9 @@ clock_rate = 1/clock_freq
 v_range=1000 # visual range, [mm]
 w_blindspot=50 # width of blindspot, [mm]
 influence_param_arr = np.linspace(.2, 1.5, 5)
-sensing_angle_arr = np.linspace(np.pi/6, np.pi, 5)
+# sensing_angle_arr = np.linspace(np.pi/6, np.pi, 5)
+sensing_angle_arr = np.linspace(15, 90, 5)
 
-#w_blindspot=3141 # TODO: figure out mapping mm to degrees
 r_sphere=50 # radius of blocking sphere for occlusion, [mm]
 n_magnitude=0.1 # visual noise magnitude, [% of distance]
 
@@ -151,55 +150,55 @@ for influence_param in influence_param_arr:
         #except:
         #    print('Provide prefix of data you want to animate in format yymmdd_hhmmss as command line argument, e.g.:\n >python animation.py 201005_111211')
         #    sys.exit()
-        try:
-            data = np.loadtxt('./logfiles/{}_data.txt'.format(filename), delimiter=',')
-            with open('./logfiles/{}_meta.txt'.format(filename), 'r') as f:
-                meta = json.loads(f.read())
-        except:
-            print('Data file with prefix {} does not exist.\nProvide prefix of data you want to animate in format yymmdd_hhmmss as command line argument, e.g.:\n >python animation.py 201005_111211'.format(filename))
-            sys.exit()
+        # try:
+        #     data = np.loadtxt('./logfiles/{}_data.txt'.format(filename), delimiter=',')
+        #     with open('./logfiles/{}_meta.txt'.format(filename), 'r') as f:
+        #         meta = json.loads(f.read())
+        # except:
+        #     print('Data file with prefix {} does not exist.\nProvide prefix of data you want to animate in format yymmdd_hhmmss as command line argument, e.g.:\n >python animation.py 201005_111211'.format(filename))
+        #     sys.exit()
 
-        # Read Experimental Parameters
-        clock_freq = meta['Clock frequency [Hz]']
-        clock_rate = 1000/clock_freq # [ms]
-        arena = meta['Arena [mm]']
-        timesteps = data.shape[0]
-        fishes = int(data.shape[1]/8)
+        # # Read Experimental Parameters
+        # clock_freq = meta['Clock frequency [Hz]']
+        # clock_rate = 1000/clock_freq # [ms]
+        # arena = meta['Arena [mm]']
+        # timesteps = data.shape[0]
+        # fishes = int(data.shape[1]/8)
 
-        # Format Data
-        x = data[:, :1]
-        y = data[:, 1:2]
-        z = data[:, 2:3]
-        phi = data[:, 3:4]
-        #vx = data[:, 4:5]
-        #vy = data[:, 5:6]
-        #vz = data[:, 6:7]
+        # # Format Data
+        # x = data[:, :1]
+        # y = data[:, 1:2]
+        # z = data[:, 2:3]
+        # phi = data[:, 3:4]
+        # #vx = data[:, 4:5]
+        # #vy = data[:, 5:6]
+        # #vz = data[:, 6:7]
 
-        for ii in range(1,fishes):
-            x = np.concatenate((x, data[:, 4*ii:4*ii+1]), axis=1)
-            y = np.concatenate((y, data[:, 4*ii+1:4*ii+2]), axis=1)
-            z = np.concatenate((z, data[:, 4*ii+2:4*ii+3]), axis=1)
-            phi = np.concatenate((phi, data[:, 4*ii+3:4*ii+4]), axis=1)
-            #vx = np.concatenate((vx, data[:, 4*(fishes+ii):4*(fishes+ii)+1]), axis=1)
-            #vy = np.concatenate((vy, data[:, 4*(fishes+ii)+1:4*(fishes+ii)+2]), axis=1)
-            #vz = np.concatenate((vz, data[:, 4*(fishes+ii)+2:4*(fishes+ii)+3]), axis=1)
+        # for ii in range(1,fishes):
+        #     x = np.concatenate((x, data[:, 4*ii:4*ii+1]), axis=1)
+        #     y = np.concatenate((y, data[:, 4*ii+1:4*ii+2]), axis=1)
+        #     z = np.concatenate((z, data[:, 4*ii+2:4*ii+3]), axis=1)
+        #     phi = np.concatenate((phi, data[:, 4*ii+3:4*ii+4]), axis=1)
+        #     #vx = np.concatenate((vx, data[:, 4*(fishes+ii):4*(fishes+ii)+1]), axis=1)
+        #     #vy = np.concatenate((vy, data[:, 4*(fishes+ii)+1:4*(fishes+ii)+2]), axis=1)
+        #     #vz = np.concatenate((vz, data[:, 4*(fishes+ii)+2:4*(fishes+ii)+3]), axis=1)
 
-        # Colors
-        v = np.sqrt(x**2 + y**2 + z**2)
-        v -= v.min(); v /= v.max()
-        colors = np.array([cm.Blues(k) for k in v])
-        #colors[:, 0, :] = cm.Reds(0.5) # this fish is red
+        # # Colors
+        # v = np.sqrt(x**2 + y**2 + z**2)
+        # v -= v.min(); v /= v.max()
+        # colors = np.array([cm.Blues(k) for k in v])
+        # #colors[:, 0, :] = cm.Reds(0.5) # this fish is red
 
-        # Create Animation
-        fig = ipv.figure()
-        ipv.xlim(0, arena[0])
-        ipv.ylim(0, arena[1])
-        ipv.zlim(0, arena[2])
-        ipv.style.use('dark')
+        # # Create Animation
+        # fig = ipv.figure()
+        # ipv.xlim(0, arena[0])
+        # ipv.ylim(0, arena[1])
+        # ipv.zlim(0, arena[2])
+        # ipv.style.use('dark')
 
-        quiver = ipv.quiver(x, y, z, np.cos(phi), np.sin(phi), np.zeros((1,len(phi))),size=6, color=colors[:,:,:3])
-        ipv.animation_control(quiver, interval=clock_rate)
+        # quiver = ipv.quiver(x, y, z, np.cos(phi), np.sin(phi), np.zeros((1,len(phi))),size=6, color=colors[:,:,:3])
+        # ipv.animation_control(quiver, interval=clock_rate)
 
-        ipv.save('./animations/{}_animation.html'.format(filename))
+        # ipv.save('./animations/{}_animation.html'.format(filename))
 
-        print('BLUEANIMAT saved your animation in ./animations/{}_animation.html.\nOpen with your favorite browser, sit back and enjoy the extravaganza!'.format(filename))
+        # print('BLUEANIMAT saved your animation in ./animations/{}_animation.html.\nOpen with your favorite browser, sit back and enjoy the extravaganza!'.format(filename))
